d5919d54564fb110586c0cbaad7003b9002a4f57
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-10-13 12:18:11 -0700

    currently tabs are working at bottom of page, still need to implement auth to map

--- src/App.js
+++ src/App.js
@@ -4,15 +4,44 @@ import { Dimensions, Image, Platform, StyleSheet, Switch, TabBarIOS, Text, Touch
 import * as Contacts from 'expo-contacts';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import Map from './components/Map.js';
 import Footer from './components/Footer.js';
 
-export default function App() {
+function HomeScreen() {
   return (
-    <>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>CURRENTLY ON "HOME"</Text>
       {/* <Map /> */}
       <Footer />
-    </>
+    </View>
   );
 }
+
+function MapScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>CURRENTLY ON "MAP SCREEN"</Text>
+      <Map />
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Login" component={HomeScreen} />
+        <Tab.Screen name="Map" component={MapScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+export default App;
