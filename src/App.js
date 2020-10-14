15af99e1e4a60ad7b2091f751e8210d8ec1754e0
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-10-14 01:53:05 -0700

    added sos button, right now it just returns an alert

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 // import AddGroupTab from './components/GroupAdd.js';
-// import MapScreen from './components/Map.js';
+import MapScreen from './components/Map.js';
 import SignUp from './components/EntryPoint.js';
 import FlatListDemo from './components/TestRenderUsers';
 
@@ -20,7 +20,7 @@ function App() {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Login" component={SignUp} />
-        {/* <Tab.Screen name="Map" component={MapScreen} /> */}
+        <Tab.Screen name="Map" component={MapScreen} />
         <Tab.Screen name="Create Group" component={FlatListDemo} />
       </Tab.Navigator>
     </NavigationContainer>
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useState } from "react";
 import MapView, { Marker, AnimatedRegion } from "react-native-maps";
 import {
+  Alert,
   Dimensions,
   Image,
   Platform,
@@ -13,16 +14,16 @@ import {
 } from "react-native";
 import * as Location from "expo-location";
 import * as Permissions from "expo-permissions";
-import socketIO from 'socket.io-client';
+import socketIO from "socket.io-client";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-
-const socket = socketIO('https://trackchat.herokuapp.com')
+const socket = socketIO("https://trackchat.herokuapp.com");
 
 const Map = () => {
   const [locationPermissions, setLocationPermissions] = useState(false);
   const [locationResult, setLocationResult] = useState("");
   const [currentLocations, setCurrentLocations] = useState({});
-  
+
   //key user: value: lat/lon
   const [everyonesPosition, setEveryonesPosition] = useState({});
 
@@ -38,50 +39,45 @@ const Map = () => {
 
   const grabLocation = (latitude, longitude) => {
     // console.log(user, 'user')
-    socket.emit('locationBroadcast', { user: 'user', latitude, longitude })
+    socket.emit("locationBroadcast", { user: "user", latitude, longitude });
     // socket.emit('locationBroadcast', { user: 'fake', latitude: 47.61625, longitude: -122.3119 })
-  }
+  };
 
   useEffect(() => {
     grantLocationPermissions();
-
   }, []);
 
   useEffect(() => {
-    socket.on('location', location => {
+    socket.on("location", (location) => {
       // console.log('location of a user:', location);
       // this is where we set everyones position
       addUsersToMap(location);
-    })
+    });
   }, []);
 
   const addUsersToMap = (location) => {
-    console.log('in add users to map', location.user)
+    console.log("in add users to map", location.user);
 
-    setEveryonesPosition(oldObj => ({
+    setEveryonesPosition((oldObj) => ({
       ...oldObj,
       [location.user]: {
-          latitude: location.latitude,
-          longitude: location.longitude
-      }
+        latitude: location.latitude,
+        longitude: location.longitude,
+      },
     }));
-  }
+  };
 
   const repeatingLocations = () => {
-
     setInterval(async () => {
-
-      console.log('in interval')
+      console.log("in interval");
       let location = await Location.getCurrentPositionAsync({});
       grabLocation(location.coords.latitude, location.coords.longitude);
-      
     }, 3000);
+  };
 
-  }
-  
   useEffect(() => {
-    console.log('every position in use effect', everyonesPosition);
-  }, [everyonesPosition])
+    console.log("every position in use effect", everyonesPosition);
+  }, [everyonesPosition]);
 
   const getStartingPosition = async () => {
     let location = await Location.getCurrentPositionAsync({});
@@ -111,19 +107,17 @@ const Map = () => {
             longitudeDelta: 0.0421,
           }}
         >
-
-          {Object.keys(everyonesPosition).map(user =>
+          {Object.keys(everyonesPosition).map((user) => (
             <Marker.Animated
               coordinate={{
                 latitude: everyonesPosition[user].latitude,
-                longitude: everyonesPosition[user].longitude
+                longitude: everyonesPosition[user].longitude,
               }}
               key={user}
               pinColor={pinColor}
               title={user}
             />
-          )}
-
+          ))}
         </MapView>
       </View>
       <View style={styles.coords}>
@@ -140,13 +134,12 @@ const Map = () => {
 // add color, add group members
 
 const getRandomColor = () => {
-  let hexcode = '#' + Math.random().toString(16).slice(2, 8);
+  let hexcode = "#" + Math.random().toString(16).slice(2, 8);
   return hexcode;
-}
+};
 let pinColor = getRandomColor();
 
 let { height, width } = Dimensions.get("window");
-
 
 const styles = StyleSheet.create({
   container: {
@@ -159,15 +152,33 @@ const styles = StyleSheet.create({
   coords: {
     height: 50,
   },
+  sos: {
+    marginTop: 65,
+    marginHorizontal: '5%',
+  },
 });
 
+function sosAlert() {
+  Alert.alert(
+    'SEND SOS'
+  )
+}
 
 function MapScreen() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>CURRENTLY ON "MAP SCREEN"</Text>
-      <Map />
-    </View>
+    <>
+      <MaterialCommunityIcons
+        name="bell-alert-outline"
+        size={50}
+        color="red"
+        style={styles.sos}
+        onPress={() => sosAlert()}
+      />
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>CURRENTLY ON "MAP SCREEN"</Text>
+        <Map />
+      </View>
+    </>
   );
 }
 
