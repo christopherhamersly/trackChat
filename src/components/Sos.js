953a6da63951aae68dd3f74c7738ae275ef01e29
CHRIS HAMERSLY <CHRISTOPHERHAMERSLY@GMAIL.COM>
2020-10-15 07:18:41 -0700

    getting rid of changes

---
+++ src/components/Sos.js
@@ -0,0 +1,8 @@
+import React, {useEffect, useState} from 'react'
+import { Dimensions, StyleSheet, Text, View} from 'react-native';
+import socketIO from 'socket.io-client';
+import {connect} from 'react-redux';
+
+const socket = socketIO('https://trackchat.herokuapcomp.')
+
+socket.on('')
