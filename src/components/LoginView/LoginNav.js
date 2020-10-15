a73dde72960a2b6c82a0119ab87289dd06ea875b
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-10-14 21:49:23 -0700

    commenting out the log in page proves that we can block the tabs from showing, this commit can example that if I can't get it tonight

--- src/components/LoginView/LoginNav.js
+++ src/components/LoginView/LoginNav.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from '@react-navigation/stack';
 
 import LogIn from './LogIn2.js';
 import SignUp from './SignUp.js';
@@ -10,20 +11,32 @@ import CreatGroup from '../CreatGroup';
 import Chat from '../Chat'
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 function LoginNav() {
   return (
-    <NavigationContainer independent={true}>
+    // <NavigationContainer independent={true}>
       <Tab.Navigator>
-        <Tab.Screen name="SignUp" component={SignUp} />
-        <Tab.Screen name="LogIn" component={LogIn} />
+        {/* <Tab.Screen name="SignUp" component={SignUp} /> */}
+        {/* <Tab.Screen name="LogIn" component={LogIn} /> */}
         <Tab.Screen name="Map" component={Map} />
         <Tab.Screen name="Create Group" component={CreatGroup} />
         <Tab.Screen name="Chat Window" component={Chat} />
       </Tab.Navigator>
+    // </NavigationContainer>
+  );
+}
+
+function HideTabBar() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* <Stack.Screen name="SignUp" component={SignUp} /> */}
+        <Stack.Screen name="userIsIn" component={LoginNav} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 
-export default LoginNav;
+export default HideTabBar;
