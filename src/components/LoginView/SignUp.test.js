3edbac6d76132d92533b977c41e88c55e363fe89
JB-Tellez <jb.tellez@gmail.com>
2020-10-16 00:48:33 -0700

    adds stqrter tests

---
+++ src/components/LoginView/SignUp.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import { Provider } from 'react-redux';
+import mockStore from '../../utils/store-mocker';
+
+import SignUp from './SignUp'
+
+it('should show sign up page', async () => {
+  const store = mockStore({
+    logReducer: {
+      username:'jane.doe', 
+      loggedIn:false
+    }
+  });
+
+  const { getByText } = render(
+    <Provider store={store}><SignUp /></Provider>
+  );
+
+  await getByText('Password:');
+});
---
+++ src/utils/store-mocker.js
@@ -0,0 +1,7 @@
+import '@testing-library/jest-dom/extend-expect'
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+export default mockStore;
